7d76b034b1f619e14d2b48fe0c1307c9d9c9e3a7
Wytamma Wirth <wytamma.wirth@me.com>
2021-08-12 13:59:47 +1000

    :art: dev script has security

--- scripts/start-dev.sh
+++ scripts/start-dev.sh
@@ -1,8 +1,8 @@
-cd backend && poetry run beastiary --debug --no-security &
+cd backend && poetry run beastiary --debug &
 cd frontend && npm run serve &
 watchmedo shell-command \
     --patterns="*.py" \
     --recursive \
-    --command="bash scripts/kill-running-server.sh && cd backend && poetry run beastiary --debug --no-security &" \
+    --command="bash scripts/kill-running-server.sh && cd backend && poetry run beastiary --debug &" \
     .
 wait
